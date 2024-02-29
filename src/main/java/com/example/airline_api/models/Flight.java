7409ce3a406de4e017aa51269aa195669544bf15
Laura Harper <lauraharper95@gmail.com>
2024-02-29 16:23:41 +0000

    annotated Flight Class with manytomany relationship

--- src/main/java/com/example/airline_api/models/Flight.java
+++ src/main/java/com/example/airline_api/models/Flight.java
@@ -1,15 +1,28 @@
 package com.example.airline_api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "Flight")
 public class Flight {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "destination")
     private String destination;
+    @Column(name = "capacity")
     private int capacity;
+    @Column(name = "departure_date")
     private String departureDate;
+    @Column(name = "departure_time")
     private String departureTime;
+    @JsonIgnoreProperties({"flights"})
+    @ManyToMany(mappedBy = "flight")
     private List<Passenger> passengers;
 
     public Flight(String destination, int capacity, String departureDate, String departureTime) {
